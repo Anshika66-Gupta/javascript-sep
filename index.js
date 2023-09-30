efa1b9f322df19fcc3ed49a5fecce3fa443a49b7
“Anshika <ansgupta.01@gmail.com>
2023-09-30 12:14:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+//a = 5
+//b = 10 
+//document.getElementById('first').innerHTML = a+b 
+//document.write(a+b )
+//window.alert("Anshika Gupta")
+//console.log(a+b)
+
+//a = 12.2
+//b = "Anshika Gupta"
+//d = true
+//c = a + b;
+//document.write(c);
+//document.write(typeof (c))
+//document.write(typeof (d))
+
+
+
+//document.write(button)
+
+
+
+//let a = 20;
+//let b = 12;
+//const c = a + b;
+
+//document.write(c)
+//var a = 12.3452345;
+//var b = 20;
+//var c = a + b;
+
+//window.alert(c)
+
+
+
+//function myExample(a, b) {
+//    return a * b;
+//}
+//let x = myExample(12, 34)
+
+//document.write(x)
+
+
+
+//const users = {
+//    firstName: 'Anshika',
+//    lastName: 'Gupta',
+//    Age: 22,
+//    Qualification: 'Software Engineer'
+//};
+//document.getElementById('first').innerHTML = users.firstName + " is a "
+// + users.Qualification  
+
+
+//const person = {
+//    firsName: 'Anshika',
+//    lastName: 'Gupta',
+//    fullName: 
+//        function () {
+//            return this.firsName + " " + this.lastName;
+//    }
+//}
+//const user = {
+//    firsName: 'Akash',
+//    lastName: 'Seervi',
+//}
+
+//let fullName = person.fullName.bind(user)
+
+//document.getElementById('first').innerHTML = fullName();
+
+
+const people = ["Anshika", "Pratim", "Akul"];
+document.getElementById('first').innerHTML = people
+function myExample() {
+    people.push("Pavan");
+}
+//let size = people.length;
+
+document.getElementById('first').innerHTML = people;
