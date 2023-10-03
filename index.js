564e9139f98e8e81f47e85551f18774ec587f6c4
“Anshika <ansgupta.01@gmail.com>
2023-10-03 18:25:58 +0530

    1-2 oct

--- index.js
+++ index.js
@@ -69,11 +69,37 @@
 //document.getElementById('first').innerHTML = fullName();
 
 
-const people = ["Anshika", "Pratim", "Akul"];
-document.getElementById('first').innerHTML = people
-function myExample() {
-    people.push("Pavan");
-}
-//let size = people.length;
+//const people = ["Anshika", "Pratim", "Akul"];
+//document.getElementById('first').innerHTML = people
+//function myExample() {
+//    people.push("Pavan");
+//}
+////let size = people.length;
 
-document.getElementById('first').innerHTML = people;
+//document.getElementById('first').innerHTML = people;
+
+
+//let length = 30.2;
+//let weight = 12.23;
+ 
+//document.write(length * weight)
+
+
+//let firstName = "Anshika";
+//let lastName = "Gupta";
+
+//document.write(firstName + " " + lastName)
+
+//let x = true;
+//let y = false;
+//document.write(typeof x);
+
+
+//let z = BigInt("12345678901234567890123456789901234567890")
+//document.write(z)
+
+
+//let a = 10
+//let b = 20
+//let c = a % b;
+//document.write(c)
